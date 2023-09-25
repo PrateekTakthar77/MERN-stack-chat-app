41350696d677b6715f3f10d15570dcaba2c3c1c1
Prateek Takthar <prateektakthar184b@gmail.com>
2023-09-25 16:08:01 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
+import { useContext } from "react";
 import Register from "./Register";
+import axios from "axios";
+import { UserContext, UserContextProvider } from "./Usercontext";
+import Routes from "./Routes";
 
 function App() {
+  axios.defaults.baseURL = "http://localhost:3030";
+  axios.defaults.withCredentials = true;
+  const { username } = useContext(UserContext);
+  console.log(username);
   return (
     <>
-      <Register />
+      <UserContextProvider>
+        <Routes />
+      </UserContextProvider>
     </>
   );
 }
 
 export default App;
+
+// 53:15
