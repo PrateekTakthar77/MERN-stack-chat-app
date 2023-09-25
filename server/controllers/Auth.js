6cea02d8f580c9fa276c4111432067bb9d6a6cf3
Prateek Takthar <prateektakthar184b@gmail.com>
2023-09-25 16:11:19 +0530

    update

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,89 +1,90 @@
 const User = require('../models/User')
-const { generateJwtToken } = require("../helpers/JWT.Verify");
+const { generateJwtToken } = require("../helpers/JWT");
 const jwt = require('jsonwebtoken');
+const JWTSECRET = process.env.JWT_SECRET
 
-const registerUser = asyncHandler(async (req, res) => {
-    try {
-        const { error } = registerUserSchema.validate(req.body);
+// const registerUser = async (req, res) => {
+//     try {
+//         const { error } = registerUserSchema.validate(req.body);
 
-        if (error) {
-            console.error(error);
-            return res.status(400).json({
-                payload: null,
-                message:
-                    error.details[0].message || "An error occurred during validation",
-            });
-        }
+//         if (error) {
+//             console.error(error);
+//             return res.status(400).json({
+//                 payload: null,
+//                 message:
+//                     error.details[0].message || "An error occurred during validation",
+//             });
+//         }
 
 
-        const { username, password, role } = req.body;
-        const userExistsWithUsername = await User.findOne({ username });
-        if (userExistsWithUsername) {
-            return res.status(409).json({
-                payload: null,
-                message: "Username already Exists.",
-            });
-        }
+//         const { username, password } = req.body;
+//         // if (userExistsWithUsername) {
+//         //     return res.status(409).json({
+//         //         payload: null,
+//         //         message: "Username already Exists.",
+//         //     });
+//         // }
 
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            host: "smtp.gmail.com",
-            port: 587,
-            secure: false,
-            auth: {
-                user: process.env.USER,
-                pass: process.env.PASSWORD,
-            },
-        });
+//         // const transporter = nodemailer.createTransport({
+//         //     service: 'gmail',
+//         //     host: "smtp.gmail.com",
+//         //     port: 587,
+//         //     secure: false,
+//         //     auth: {
+//         //         user: process.env.USER,
+//         //         pass: process.env.PASSWORD,
+//         //     },
+//         // });
 
-        const mailOptions = {
-            from: {
-                name: 'Jewellery Bliss',
-                address: process.env.USER
-            }, // sender address
-            to: email, // list of receivers
-            subject: "Welcome to jewellery Bliss", // Subject line
-            text: "Welcome to jewellery Bliss your Registration has been sucessfull, enjoy your time on our APP", // plain text body
-            // html: "<b>Hello world?</b>", // html body
-        };
+//         // const mailOptions = {
+//         //     from: {
+//         //         name: 'Jewellery Bliss',
+//         //         address: process.env.USER
+//         //     }, // sender address
+//         //     to: email, // list of receivers
+//         //     subject: "Welcome to jewellery Bliss", // Subject line
+//         //     text: "Welcome to jewellery Bliss your Registration has been sucessfull, enjoy your time on our APP", // plain text body
+//         //     // html: "<b>Hello world?</b>", // html body
+//         // };
 
-        const sendMail = async (transporter, mailOptions) => {
-            try {
-                await transporter.sendMail(mailOptions)
-                console.log("Mail Sent succesfully")
-            } catch (error) {
-                console.log(error);
-            }
-        }
+//         // const sendMail = async (transporter, mailOptions) => {
+//         //     try {
+//         //         await transporter.sendMail(mailOptions)
+//         //         console.log("Mail Sent succesfully")
+//         //     } catch (error) {
+//         //         console.log(error);
+//         //     }
+//         // }
 
-        sendMail(transporter, mailOptions)
-        const hashedPassword = await bcrypt.hash(password, 10);
+//         // sendMail(transporter, mailOptions)
+//         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const newUser = await User.create({
-            username,
-            password: hashedPassword,
-            role,
-            userCount: ((await User.find({}))?.length ?? 0) + 1,
-        });
+//         const newUser = await User.create({
+//             username,
+//             password: hashedPassword,
+//             // role,
+//             // userCount: ((await User.find({}))?.length ?? 0) + 1,
+//         });
 
-        const payload = {
-            username: newUser.username,
-            userCount: newUser.userCount,
-        };
-        const token = generateJwtToken(payload);
-        return res.cookie('tokken', token).status(201).json({
-            message: "User created successfully",
-            payload,
-            userId: newUser.id,
-            token,
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            payload: null,
-            message: error.message || "An error occurred",
-        });
-    }
-});
+//         const payload = {
+//             username: newUser.username,
+//             // userCount: newUser.userCount,
+//             password: newUser.password
+//         };
+//         const token = generateJwtToken(payload);
+//         return res.cookie('tokken', token).status(201).json({
+//             message: "User created successfully",
+//             payload,
+//             userId: newUser.id,
+//             token,
+//         });
+//     } catch (error) {
+//         console.error(error);
+//         return res.status(500).json({
+//             payload: null,
+//             message: error.message || "An error occurred",
+//         });
+//     }
+// };
 
-module.exports = registerUser;
+// module.exports = registerUser;
--- server/index.js
+++ server/index.js
@@ -1,18 +1,47 @@
 const express = require('express');
-require('dotenv').config();
+const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
+const jwt = require('jsonwebtoken'); // Import jwt library
+const User = require('./models/User');
+
+// Load environment variables
+require('dotenv').config();
 
 const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 3030;
+const JWT_SECRET = process.env.JWT_SECRET;
 
-const app = express();
+// Define CORS options
+const corsOptions = {
+    credentials: true,
+    origin: 'http://localhost:5173' // Update the origin without the trailing slash
+};
 
-app.get('/', (req, res, next) => {
+// Use 'cors' middleware with options
+app.use(cors(corsOptions));
+
+app.use(express.json()); // Middleware for parsing JSON request bodies
+
+app.get('/', (req, res) => {
     res.status(200).json({
         ok: true,
         message: 'working'
-    })
-})
+    });
+});
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const createdUser = await User.create({ username, password });
+        jwt.sign({ userId: createdUser._id }, JWT_SECRET, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).status(201).json({ id: createdUser._id });
+        });
+    } catch (error) {
+        res.status(500).json(`error`, error);
+    }
+});
 
 mongoose.connect(MONGO_URL, {
     useNewUrlParser: true,
