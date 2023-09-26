e6541ba38be16edf9544c89665b18045e0848901
Prateek Takthar <prateektakthar184b@gmail.com>
2023-09-26 14:52:32 +0530

    update

--- client/src/Usercontext.jsx
+++ client/src/Usercontext.jsx
@@ -1,10 +1,16 @@
-import { createContext, useState } from "react";
-
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
   const [username, setUsername] = useState(null);
   const [id, setId] = useState(null);
+  useEffect(() => {
+    axios.get("/profile").then((Response) => {
+      setId(Response.data.userId);
+      setUsername(Response.data.username);
+    });
+  });
   return (
     <UserContext.Provider value={{ username, setUsername, id, setId }}>
       {children}
